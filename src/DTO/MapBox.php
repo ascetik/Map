84a90a70f98be76cdf200221163b8a06419f9058
ascetik <ascetik@free.fr>
2023-10-28 11:17:13 +0200

    MapBox is composed with a Box instead of extending
    to avoid offset direct access

--- src/DTO/MapBox.php
+++ src/DTO/MapBox.php
@@ -3,24 +3,105 @@
 namespace Ascetik\Map\DTO;
 
 use Ascetik\Box\Box;
-use Ascetik\ContainerExt\keys\StringKey;
-use Ascetik\ContainerExt\Values\Content;
+use Ascetik\ContainerExt\Keys\StringKey;
+use Ascetik\ContainerExt\Types\ContainerKey;
+use Ascetik\ContainerExt\Types\ContainerValue;
+use Ascetik\ContainerExt\Values\MixedValue;
 
-class MapBox extends Box
+class MapBox
 {
-    public function hasValue(mixed $value): bool
+    /**
+     * @var Box<ContainerKey,ContainerValue>
+     */
+    private Box $box;
+
+    public function __construct()
     {
-        $found = $this->find(
-            function(StringKey $key) use ($value){
-                $content = $this->getValueFrom($key);
-                return $content->value == $value;
+        $this->box = new Box();
+    }
+
+    public function content():ReadonlyMap
+    {
+        return new ReadonlyMap($this->box);
+    }
+
+    public function hasValue(mixed $content): bool
+    {
+        $found = $this->box->find(
+            function (StringKey $key) use ($content) {
+                $value = $this->getValueFrom($key);
+                return $value->content() == $content;
             }
         );
         return !is_null($found);
     }
 
-    private function getValueFrom(StringKey $key):Content
+    public function hasKey(string $key)
     {
-        return $this->offsetGet($key);
+        $found = $this->getKey($key);
+        return !is_null($found);
+    }
+
+    public function valueOf(string $key)
+    {
+        return $this->getValueFrom($this->getKey($key))->content();
+    }
+
+    public function setValue(string $key, mixed $value)
+    {
+        if ($this->hasValue($value)) {
+            return;
+        }
+
+        $content = new MixedValue($value);
+
+        if ($Key = $this->getKey($key)) {
+            $this->box->offsetSet($Key, $content);
+            return;
+        }
+
+        $this->box->attach(new StringKey($key), $content);
+    }
+
+    public function delete(string $name)
+    {
+        $key = $this->getKey($name);
+        $this->box->detach($key);
+    }
+
+    public function getKeys():array
+    {
+        $keys = [];
+        $this->box->each(
+            function(StringKey $key) use (&$keys){
+                $keys[] = $key->id();
+            }
+        );
+        return $keys;
+
+    }
+
+    public function getValues():array
+    {
+        $values = [];
+        $this->box->each(
+            function(StringKey $key) use (&$values){
+                $values[] = $this->box->offsetGet($key)->content();
+            }
+        );
+        return $values;
+
+    }
+
+    private function getKey(string $key): ?StringKey
+    {
+        return $this->box->find(
+            fn (StringKey $Key) => $Key->id() == $key
+        );
+    }
+
+    private function getValueFrom(StringKey $key): MixedValue
+    {
+        return $this->box->offsetGet($key);
     }
 }
